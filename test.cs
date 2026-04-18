04c3ff8c8bc5217693dcd73bbeeb031a76d8c88f
Veronika <115186505+nikiveron@users.noreply.github.com>
2026-04-18 16:06:40 +0400

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,33 +1,52 @@
-public class KafkaConsumerService : BackgroundService
-{
-    private readonly IConfiguration _configuration;
+  zookeeper:
+    image: confluentinc/cp-zookeeper:7.5.0
+    container_name: zookeeper
+    environment:
+      ZOOKEEPER_CLIENT_PORT: 2181
+    networks:
+      - linktracker-network
 
-    public KafkaConsumerService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
+  kafka-1:
+    image: confluentinc/cp-kafka:7.5.0
+    container_name: kafka-1
+    depends_on:
+      - zookeeper
+    ports:
+      - "9092:9092"
+    environment:
+      KAFKA_BROKER_ID: 1
+      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
+      KAFKA_ADVERTISED_LISTENERS: PLAINTEXT://kafka-1:9092
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 3
+    networks:
+      - linktracker-network
 
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        var config = new ConsumerConfig
-        {
-            BootstrapServers = _configuration["Kafka:BootstrapServers"],
-            GroupId = "bot-consumer-group",
-            AutoOffsetReset = AutoOffsetReset.Earliest
-        };
+  kafka-2:
+    image: confluentinc/cp-kafka:7.5.0
+    container_name: kafka-2
+    depends_on:
+      - zookeeper
+    ports:
+      - "9093:9093"
+    environment:
+      KAFKA_BROKER_ID: 2
+      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
+      KAFKA_ADVERTISED_LISTENERS: PLAINTEXT://kafka-2:9093
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 3
+    networks:
+      - linktracker-network
 
-        var consumer = new ConsumerBuilder<string, string>(config).Build();
-        consumer.Subscribe(_configuration["Kafka:Topic"]);
-
-        return Task.Run(() =>
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                var result = consumer.Consume(stoppingToken);
-                var update = JsonSerializer.Deserialize<LinkUpdateDto>(result.Message.Value);
-
-                // вызываешь свою бизнес-логику отправки уведомления
-            }
-        }, stoppingToken);
-    }
-}
+  kafka-3:
+    image: confluentinc/cp-kafka:7.5.0
+    container_name: kafka-3
+    depends_on:
+      - zookeeper
+    ports:
+      - "9094:9094"
+    environment:
+      KAFKA_BROKER_ID: 3
+      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
+      KAFKA_ADVERTISED_LISTENERS: PLAINTEXT://kafka-3:9094
+      KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR: 3
+    networks:
+      - linktracker-network
